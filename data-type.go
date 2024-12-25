d740b99794945aa7593189d99a479c7aadbcfa33
Timothy <timothymang09@gmail.com>
2024-12-25 01:34:46 -0500

    datatypes in go

---
+++ data-type.go
@@ -0,0 +1,9 @@
+intergers 
+		/*(int) meaning signed integers this includes +ives and -ves integers 1344 3 533 
+		(uint)meaning unsigned integers are +ves integers.
+		*/
+floats 0.034 5.63 
+
+strings "name" "kola bear"
+
+boolean true false
